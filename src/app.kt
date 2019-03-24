99d660d9d7addc90825343a6596d95260c494130
Stephen Merrony <SMerrony@users.noreply.github.com>
2019-03-24 18:57:23 +0100

    Add copyright, minor clean-ups.

--- src/app.kt
+++ src/app.kt
@@ -1,5 +1,30 @@
 @file:UseExperimental(ExperimentalUnsignedTypes::class)
 
+/*
+MIT License
+
+Copyright (c) 2019 Stephen Merrony
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
 import java.io.BufferedInputStream
 import java.io.File
 import java.nio.file.Files
@@ -90,7 +115,7 @@ fun main(args: Array<String>) {
     while (!done) {
         val recHdr = readHeader(bufferedDump)
         if (verbose) {
-            println("Found block of type: " + recHdr.recordType.name + " length: ${recHdr.recordLength}")  // TODO how to show enum name here?
+            println("Found block of type: " + recHdr.recordType.name + " length: ${recHdr.recordLength}")
         }
         when (recHdr.recordType) {
             RecordType.FSB -> {
@@ -102,7 +127,7 @@ fun main(args: Array<String>) {
             }
             RecordType.UDA -> {
                 // throw away for now
-                val udaBlob =  readBlob(recHdr.recordLength, bufferedDump, "UDA")
+                readBlob(recHdr.recordLength, bufferedDump, "UDA")
             }
             RecordType.ACL -> {
                 var aclBlob =  readBlob(recHdr.recordLength, bufferedDump, "ACL")
@@ -186,7 +211,7 @@ fun readHeader(d: BufferedInputStream): RecordHeader {
         exitProcess(1)
     }
     val rt = twoBytes[0].toInt().shr(2) and 0x00FF
-    val rl = (twoBytes[0].toInt() and 0x03).shl(8) + twoBytes[1].toInt() // TODO Test this!
+    val rl = (twoBytes[0].toInt() and 0x03).shl(8) + twoBytes[1].toInt()
     return RecordHeader(RecordType.fromInt(rt), rl)
 }
 
